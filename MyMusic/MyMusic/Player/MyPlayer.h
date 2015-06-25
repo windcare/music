d05b05c10efb18ce1244e80b1621ff7c042d0414
sjjwind <xq.qxq@alibaba-inc.com>
2015-06-25 16:25:59 +0800

    modify myplayer.h

---
+++ MyMusic/MyMusic/Player/MyPlayer.h
@@ -0,0 +1,38 @@
+//
+//  MyPlayer.h
+//  MyMusic
+//
+//  Created by sjjwind on 5/14/15.
+//  Copyright (c) 2015 sjjwind. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "MusicFile.h"
+
+@protocol MyPlayerDelegate <NSObject>
+
+// play
+- (void)onPlaying;
+- (void)onPaused;
+- (void)onStoped;
+- (void)onIdle;
+- (void)onFinished;
+- (void)onError;
+
+@end
+
+@interface MyPlayer : NSObject
+
+@property (nonatomic, strong) id<MyPlayerDelegate> delegate;
+
+- (void)play:(MusicFile *)music;
+- (void)resume;
+- (void)pause;
+- (void)stop;
+- (void)setVolumn:(double)value;
+- (double)getVolumn;
+- (void)setProgress:(NSTimeInterval)progress;
+- (NSTimeInterval)getProgress;
+- (NSInteger)getTotalTime;
+
+@end
