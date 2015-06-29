b3d4b1a1e14e07f813af557882bc2ecc503ecc92
sjjwind <sjjwind@live.com>
2015-06-29 19:24:51 +0800

    modify web

--- Server/src/controller/test.go
+++ Server/src/controller/test.go
@@ -23,7 +23,7 @@ func NewTestController() *TestController {
 func (this *TestController) TestAction(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	var info NetInfo
-	info.Host = "http://" + config.ConfigManagerInstance().ReadIPAddress()
+	info.Host = config.ConfigManagerInstance().ReadIPAddress()
 	info.Port = config.ConfigManagerInstance().ReadPort()
 	t, err := template.ParseFiles("./src/view/test.html")
 	if err != nil {
