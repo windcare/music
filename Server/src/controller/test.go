8e95f3ef5cf0ac1f1f647669b7b510fbb66fa1fb
sjjwind <sjjwind@live.com>
2015-06-29 19:17:12 +0800

    modify web

--- Server/src/controller/test.go
+++ Server/src/controller/test.go
@@ -1,13 +1,17 @@
 package controller
 
 import (
+	"config"
 	"fmt"
-	// "github.com/bitly/go-simplejson"
-	// "errors"
 	"html/template"
 	"log"
 	"net/http"
 )
+
+type NetInfo struct {
+	Host string
+	Port int
+}
 
 type TestController struct {
 }
@@ -18,10 +22,12 @@ func NewTestController() *TestController {
 
 func (this *TestController) TestAction(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
-	fmt.Println(r.URL.Path)
+	var info NetInfo
+	info.Host = "http://" + config.ConfigManagerInstance().ReadIPAddress()
+	info.Port = config.ConfigManagerInstance().ReadPort()
 	t, err := template.ParseFiles("./src/view/test.html")
 	if err != nil {
 		log.Println(err)
 	}
-	t.Execute(w, nil)
+	t.Execute(w, &info)
 }
